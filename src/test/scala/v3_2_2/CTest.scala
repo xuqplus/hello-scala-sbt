d31cae5c97c14aa1af6aec84681234a807d33ca1
Qunqun Xu <qunqun.xu@autodesk.com>
2021-01-26 16:17:21 +0800

    Test two non-repeating numbers in an array

--- src/test/scala/v3_2_2/CTest.scala
+++ src/test/scala/v3_2_2/CTest.scala
@@ -8,4 +8,21 @@ class CTest extends AnyFunSuite {
     info("" + (1 ^ 2))
     assert((1 ^ 2) == 3)
   }
+
+  val arr = Array(1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 7, 8, 8, 9, 9)
+
+  test("two non-repeating numbers in an array") {
+    var c: Integer = null
+    for (a <- arr) {
+      if (c == null)
+        c = a
+      else
+        c ^= a
+      //      for (b <- arr) {
+      //        info(String.format("%s-%s", a, b))
+      //      }
+    }
+    info("" + c)
+    info("" + (5 ^ 6))
+  }
 }
